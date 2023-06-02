47edf1aae00d63b7cfae74f18fe1fd07fcc38ae9
Philip-D21 <philiplekan88@gmail.com>
2023-06-02 21:47:58 +0100

    okay

--- controller/bookController.js
+++ controller/bookController.js
@@ -14,9 +14,66 @@ async function getAllBooks (req,res,next){
     }
 }
 
+async function getBookById(req, res, next) {
+    try {
+        const book = await Book.findByPk(req.params.id)
+        res.json(book)
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function addBook(req, res, next) {
+    let bookInfo = req.body;
+    try {
+        const book = await Book.create(bookInfo);
+        res.status(201).json(book);
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+async function updateBook(req, res, next) {
+    try {
+        const book = await Book.findByPk(req.params.id);
+        if (book) {
+            await book.update(req.body);
+            res.json(book);
+        } else {
+            res.status(404).json({
+                message: 'Book not found'
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function deleteBook(req, res, next) {
+    try {
+        const book = await Book.findByPk(req.params.id);
+        if (book) {
+            await book.destroy();
+            res.json(book);
+        } else {
+            res.status(404).json({
+                message: 'Book not found'
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+
 
 
 
 module.exports = {
-    getAllBooks
+    getAllBooks,
+    getBookById,
+    addBook,
+    updateBook,
+    deleteBook
 }
--- routes/book.js
+++ routes/book.js
@@ -2,12 +2,19 @@ const express = require("express");
 const router  = express.Router();
 
 const {
-    getAllBooks 
+    getAllBooks,
+    addBook,
+    getBookById,
+    updateBook,
+    deleteBook
 } = require("../controller/bookController");
 
 
 router.get("/", getAllBooks);
-
+router.post("/", addBook);
+router.get("/:id", getSingleBook);
+router.put("/:id",updateBook);
+router.delete("/:id", deleteBook);
 
 
 module.exports = router;
